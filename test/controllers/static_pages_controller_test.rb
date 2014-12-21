a79f919a48f9251f6fab200ba3e8e3f51d831be1
Page Piccinini <page.piccinini@gmail.com>
2014-12-21 14:48:16 +0100

    Add a Contact page (solve exercise 3.2)

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -24,4 +24,9 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_select "title", "About | #{@base_title}"
   end
 
+  test "should get contact" do
+    get :contact
+    assert_response :success
+  end
+
 end
