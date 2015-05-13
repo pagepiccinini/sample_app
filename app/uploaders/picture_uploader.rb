b36b591e56f900b14ec34aab3fc59d102f0b7010
Page Piccinini <page.piccinini@gmail.com>
2015-05-13 17:42:59 +0200

    Small update to photo uploads

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -7,7 +7,7 @@ class PictureUploader < CarrierWave::Uploader::Base
   #if Rails.env.production?
   #  storage :fog
   #else
-  #  storage :file
+  storage :file
   #end
 
   # Override the directory where uploaded files will be stored.
